6b04f8fff34c669ffa761fb11529109d61e6127d
jiahuu <729161752@qq.com>
2021-07-08 10:34:56 +0800

    slf4j

---
+++ Slf4jLog/src/main/java/com/qdd/slf4jlog/Slf4jLogApplication.java
@@ -0,0 +1,13 @@
+package com.qdd.slf4jlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4jLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slf4jLogApplication.class, args);
+    }
+
+}
